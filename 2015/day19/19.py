3b64121f329dc2ccae05b19dc3f3b164c9440fd7
jmcdonough98 <joe.mcdonough@live.com>
2024-12-01 16:28:18 -0600

    2015 Day 19, 2015 Complete

--- 2015/day19/19.py
+++ 2015/day19/19.py
@@ -1,81 +1,32 @@
-from collections import defaultdict
-import time
-def parseInput(s):
-    tmp = s.split('\n\n')
-    formulas = defaultdict(list)
+import re
+from collections import Counter
+lines = open('input.txt','r').read().split('\n')
 
-    for l in tmp[0].split('\n'):
-        left, right = l.split(' => ')
-        formulas[left].append(right)
-    return formulas, tmp[1]
+rules = [x.split(' => ') for x in lines[:-2]]
+molecule = lines[-1]
 
-def part1(formulas, inputMolecule):
-    outputs = set()
-    for i in range(len(inputMolecule)):
-        if inputMolecule[i] in formulas:
-            for output in formulas[inputMolecule[i]]:
-                outputs.add(inputMolecule[:i] + output + inputMolecule[i+1:])
-        tmp = inputMolecule[i:i+2]
-        if tmp in formulas and len(tmp) == 2:
-            for output in formulas[tmp]:
-                outputs.add(inputMolecule[:i] + output + inputMolecule[i+2:])
-    return outputs
 
-def part2Stack(outs, backFormulas, inputMol):
-    stack = ""
-    
+def applyrule(molecule, idx, old, new):
+    return molecule[:idx] + new + molecule[idx+len(old):]
 
-def part2(formulas,inputMol, target):
-    outs = []
-    backFormulas = {}
-    for x in formulas:
-        for y in formulas[x]:
-            outs.append(y)
-            backFormulas[y] = x
-    outs = sorted(outs, key = lambda x: len(x),reverse=True)
-    i = 1
-    return part2Recursive(i, outs,backFormulas, inputMol)
-    # while inputMol != 'e': #and i < 202:
-    #     print(len(inputMol), inputMol)
-    #     for o in outs:
-    #         try:
-    #             tmp = inputMol.index(o)
-    #             inputMol = inputMol[:tmp] + backFormulas[o] + inputMol[tmp+len(o):]
-    #             i += 1
-    #             break
-    #         except:
-    #             continue
-    # print(backFormulas)
-    # print(i)
-backtrack = set()
-def part2Recursive(i,outs, backFormulas, inputMol):
-    if i > 205:
-        print(i)
-    for o in outs:
-        try:
-            tmp = inputMol.index(o)
-            modifiedMol = inputMol[:tmp] + backFormulas[o] + inputMol[tmp+len(o):]
-            if modifiedMol == 'e':
-                return i
-            print(i, modifiedMol)
-            
-            if modifiedMol in backtrack:
-                return None
-            backtrack.add(modifiedMol)
-            # time.sleep(0.1)
-            tmp2 = part2Recursive(i+1,outs,backFormulas,modifiedMol)
-            
-            
-            if tmp2 != None:
-                return tmp2
-        except:
-            continue
-    # backtrack.append(inputMol)
-    return None
+def part1(rules, molecule):
+    newMolecules = set()
+    for (old, new) in rules:
+        for i in range(len(molecule)):
+            if molecule[i:i+len(old)] == old:
+                newMolecules.add(applyrule(molecule, i,old, new))
+    return len(newMolecules)
 
-s = open('input.txt','r').read()
-formulas, inputMolecule = parseInput(s)
-# print("Part1:", len(part1(formulas,inputMolecule)))
-print("Part2:", part2(formulas, inputMolecule, 'e'))
-# print('abbbb'.index('bab'))
-#195 = too low
+
+def part2(molecule):
+    molecule = re.sub(r"O|P|B|Mg|Ca|Si|Th|F|B|Al|Ti|C|H", "A", molecule)
+    molecule = re.sub(r"Rn|Ar", "B", molecule)
+    molecule = re.sub(r"Y", "C", molecule)
+    c = Counter(molecule)
+    return len(molecule) - c["B"] - 2*c["C"] - 1
+
+print(part1(rules, molecule))
+print(part2(molecule))
+
+
+
